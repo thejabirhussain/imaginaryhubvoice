bb9b04caa5d849a5085b40be732f22bd02b9d373
Shaik Mohammed Jabir Hussain <118990335+thejabirhussain@users.noreply.github.com>
2025-05-07 01:40:04 +0530

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { useLayoutEffect, useState } from "react";
-import HumeLogo from "./logos/Hume";
 import { Button } from "./ui/button";
 import { Moon, Sun } from "lucide-react";
-import Github from "./logos/GitHub";
-import pkg from '@/package.json';
 
 export const Nav = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -32,26 +29,8 @@ export const Nav = () => {
         "px-4 py-2 flex items-center h-14 z-50 bg-card border-b border-border"
       }
     >
-      <div>
-        <HumeLogo className={"h-5 w-auto"} />
-      </div>
+      <div className="text-lg font-semibold">Imaginary Hub</div>
       <div className={"ml-auto flex items-center gap-1"}>
-        <Button
-          onClick={() => {
-            window.open(
-              pkg.homepage,
-              "_blank",
-              "noopener noreferrer"
-            );
-          }}
-          variant={"ghost"}
-          className={"ml-auto flex items-center gap-1.5"}
-        >
-          <span>
-            <Github className={"size-4"} />
-          </span>
-          <span>Star on GitHub</span>
-        </Button>
         <Button
           onClick={toggleDark}
           variant={"ghost"}
